c3f8b28da58e09905d96353a380b6c81ef8f2d29
IsaikinSergei <akinfest007@gmail.com>
2020-12-13 21:13:20 +0400

    Init

---
+++ PhotoTapps/PhotoCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Sergei Isaikin on 13.12.2020.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var dogImageView: UIImageView!
+    
+    
+    
+}
